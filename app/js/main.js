4b4ef16a54adae2d6f64e23b263d2db5c58ec826
yanam-dev <yamusjko@gmail.com>
2019-12-05 12:52:13 +0100

    added property-slider section and projects-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,52 @@
 $(function(){
+// tabs
+$('.search__tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+    $('.search__form').find('.search__form-item').removeClass('active-tab').hide();
+    $('.search__tabs').find('.search__tab').removeClass('active');
+    $(this).addClass('active');
+    $('#'+id).addClass('active-tab').fadeIn();
+    return false;
+});
+
+// menu button
+$('.menu__btn').on('click', function (){
+    $('.menu__list').slideToggle();
+});
+
+// Select Styler
+$('input, select').styler();
+
+// Price Range block
+$(".js-range-slider").ionRangeSlider({
+    skin: "square",
+    type: "double",
+    grid: false,
+    prefix: "Bedroom ",
+    max_postfix: "+",
+});
+// slider
+$('.property__slider, .project__slider, .partners__slider').slick({
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev slick-arrow slider-btn" alt="Link to..."></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow slider-btn" alt="Link to..."></button>',
+
+});
 
 
-
+// $('.property__slider-icon').on('click', function () {
+//     $('.property__slider-card').addClass('active');
+//     $(".icon-th-list").addClass('active');
+//     $(".property__slider-card").removeClass('active');
+// });
+// $('.icon-th-large').on('click', function () {
+//     $('.products__item').removeClass('list');
+//     $('.icon-th-large').addClass('active');
+//     $('.icon-th-list').removeClass('active');
+// });
     
 });
--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,10 @@ gulp.task('style', function() {
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -43,7 +46,9 @@ gulp.task('style', function() {
 gulp.task('script', function() {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
